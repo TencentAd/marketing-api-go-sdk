0933d03b8ecc0005a0120ee900991a709359cc0f
tencent_ad_developer <tencent_ad_developer@tencent.com>
2022-04-02 09:25:00 +0800

    v1.7.19

--- pkg/ads/tencentads.go
+++ pkg/ads/tencentads.go
@@ -25,7 +25,7 @@ type SDKClient struct {
 
 // Init ...
 func Init(cfg *config.SDKConfig) *SDKClient {
-	version := "1.7.18"
+	version := "1.7.19"
 	apiVersion := "v1.1"
 	ctx := context.Background()
 	nonce := uuid.New().String()
